ca132f59af62a0949d0ec4925c5e03f8208abee7
kwing25 <kendracwing@icloud.com>
2022-10-28 17:43:48 -0600

    Working on components and styles In progress..

--- gs-angle/src/components/slide-container.jsx
+++ gs-angle/src/components/slide-container.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 // import { useLottie } from "lottie-react";
 import Summary from "./slides/Summary";
-import Problem from "./slides/Problem";
-import Solution from "./slides/Solution";
-import Compeition from "./slides/Compeition";
-import Revenue from "./slides/Revenue";
-import Privacy from "./slides/Privacy";
-import PlansFeatures from "./slides/PlansFeatures";
-import Team from "./slides/Team";
-import Advantages from "./slides/Advantages";
+// import Problem from "./slides/Problem";
+// import Solution from "./slides/Solution";
+// import Compeition from "./slides/Compeition";
+// import Revenue from "./slides/Revenue";
+// import Privacy from "./slides/Privacy";
+// import PlansFeatures from "./slides/PlansFeatures";
+// import Team from "./slides/Team";
+// import Advantages from "./slides/Advantages";
 
 import "./slide-container-styles.scss";
 
@@ -16,14 +16,14 @@ export default function SlideContainer() {
     return (
         <div className="pitch-wrapper ">
             <Summary />
-            <Problem />
-            <Solution />
-            <Compeition />
-            <Revenue />
-            <Privacy />
-            <PlansFeatures />
-            <Team />
-            <Advantages />
+            {/* <Problem /> */}
+            {/* <Solution /> */}
+            {/* <Compeition /> */}
+            {/* <Revenue /> */}
+            {/* <Privacy /> */}
+            {/* <PlansFeatures /> */}
+            {/* <Team /> */}
+            {/* <Advantages /> */}
         </div>
     );
 }
