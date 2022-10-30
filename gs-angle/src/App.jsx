4341053a72b93b2636907eed2b59804441fe1e23
kwing25 <kendracwing@icloud.com>
2022-10-30 16:08:46 -0600

    Working on temp files...

--- gs-angle/src/App.jsx
+++ gs-angle/src/App.jsx
@@ -3,12 +3,14 @@
 import "./styles/App.scss";
 // import SlideContainer from "./components/slide-container";
 import TempSlideContainer from "./temp/temp-slide-container";
+import TempMainContainer from "./temp/main";
 
 function App() {
   return (
     <div className="App">
       {/* <SlideContainer /> */}
-      <TempSlideContainer />
+      {/* <TempSlideContainer /> */}
+      <TempMainContainer />
     </div>
   );
 }
